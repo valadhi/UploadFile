cdda8b62208ac556c6d46f0634a2e64184e1e440
valadhi <vlad@replay.local>
2013-06-19 15:19:01 +0100

    Wednesday

--- uploadFile.js
+++ uploadFile.js
@@ -1,11 +1,16 @@
+'use strict';
+
 // for new webkit browsers, the .slice() method is named .webkitSlice()
 // similar for mozilla
 File.prototype.slice = File.prototype.webkitSlice || File.prototype.mozSlice || File.prototype.slice;
 
-var UploadItem = function(file){
+var UploadItem = function(config){
+	
+	this.delegate = config.delegate; 
+	this.form = config.form;
+	this.ID = config.id;
 	
 	this.file = null;
-	this.ID = 0;
 	this.state = null;
 	this.file_name = null;
 	this.size = 0;
@@ -13,15 +18,19 @@ var UploadItem = function(file){
 	this.totalSlices = 0;
 	this.uploadedSlices = 0; 
 	
-	this.start(file);
+	//this.start(file);
+
+	this.renderForm();
+	this.clickHandlers();
 };
 
 
 UploadItem.prototype = {
 	start : function(file){
 		// Initialise click handlers!
-		this.clickHandlers();
+		//this.clickHandlers();
 		this.file = file;
+		//alert(this.ID);
 		var jason;//response from server
 		var xhr;
 		var fd;
@@ -37,14 +46,14 @@ UploadItem.prototype = {
 
 		this.file_name = this.file.name;
 		console.log('File:');
-		console.log(file);
+		console.log(this.file);
 		this.totalSlices = Math.ceil(this.file.size/this.chunk_size);
 		
 		fd.append("name", this.file_name);
 		fd.append("chunk_size", this.chunk_size);
 		fd.append("size", this.file.size);
 		
-		state = 'uploading';
+		this.state = 'uploading';
 		//this.uploads[id] = {'status' : 'uploading' };
 		
 		var self = this;
@@ -54,16 +63,17 @@ UploadItem.prototype = {
 				if(jason == 0){
 					console.log('File not found; start anew');
 					console.log("Uploading file " + self.file.name + " of size " + self.file.size + " bytes...");
-					self.uploading = true;
+					self.state = "uploading";
 					self.UploadFile();
 				}else{
 					if(jason == self.totalSlices){
-						alert(jason + " " + self.totalSlices + " " + this.file.size);
+						alert(jason + " " + self.totalSlices + " " + self.file.size);
 						console.log("File already exists");
+						//this.delegate.fileCount--;
 					}
 					else{
 						console.log('Found partial file; resuming....');
-						self.uploading = true;
+						self.state = "uploading";
 						self.uploadedSlices = jason;
 						//alert(this.uploadedSlices);
 						self.UploadFile();
@@ -83,15 +93,23 @@ UploadItem.prototype = {
 
 			this.UploadChunk(myBlob,this.uploadedSlices);
 		}else{
-			this.uploadedSlices = 0;
-			output.innerHTML = output.innerHTML + "File uploaded successfully! <br>";
-			console.log("File uploaded successfully");
-			
+			this.complete();			
 		}
-},
+	},
+	
+	complete: function() {
+		this.uploadedSlices = 0;
+		output.innerHTML = output.innerHTML + "File uploaded successfully! <br>";
+		console.log("File uploaded successfully");
+		this.state = "done";
+		
+		this.delegate.complete(this);
+		
+		
+	},
 
 UploadChunk: function (bFile, chunk){
-	if(this.uploading){
+	if(this.state === "uploading"){
 		var data = new FormData();
 		
 		data.append('file', bFile);
@@ -134,8 +152,56 @@ UploadChunk: function (bFile, chunk){
 		state = 'aborted';
 	},
 	
-	clickHandlers : function(){
+	clickHandlers : function() {
 		
+		if (!this.form) {
+			return;
+		}
+		
+		var self = this;
+		this.form.find('input[name="upload"]').click(function() {
+			
+			self.onClick();
+		});
+		
+
+		
+		//this.form.find('input[name="upload"]').click($.proxy(this.onClick, this));
+	},
+	
+	onClick: function() {
+		
+		console.log('Upload ITEM Click on id');
+		console.log(this.ID);
+		
+		var fileInput = this.form.find('input[name="file"]')[0];
+		var files = fileInput.files;
+		console.log(files);
+		
+		return;
+		
+		
+		var currID = $("#uploadButton").siblings().attr('rel:fileInput');
+		alert(currID);
+	//	var fileInput =  document.getElementById("inputFile-" + currID);
+		var files = fileInput.files;
+		if (files.length === 0){
+			console.log('No input file found');
+			return;
+		}
+		var file = files[0];
+		this.delegate.queue(file);
+	},
+	
+	renderForm: function() {
+
+		this.form = $('<form  enctype="multipart/form-data" method="post" name="fileinfo"></form>');
+		
+		this.form.html('<label>File to upload:</label><input type="file" name="file" required />' +
+				  '<div class="output"></div><input type="button" name="upload" value="Upload File" />'+
+				  '<span class="stopButton">Stop</span><span id="addUpload" style="color:red">Add upload form</span>');
+		
+		$('#upload').append(this.form);
 	}
 };
 
@@ -146,6 +212,8 @@ var UploadController = function() {
 	//this.file_name = null;
 	//this.uploading = null;
 	//this.init(file);
+	this.fileCount = 0;
+	
 	
 	this.uploads = [];
 	
@@ -153,20 +221,27 @@ var UploadController = function() {
 	UploadController.prototype = {
 			
 			queue : function(file){
-				var uploadItem = new UploadItem(file);
-				this.uploads[0] = uploadItem;
+				this.file = file;
+				//if(this.fileCount == 0)
+			
+				//var self = this;
+				var uploadItem = new UploadItem(this, this.fileCount);
+				
+				this.fileCount++;
+				//alert(this.fileCount);
+				//alert("upload ID " + this.fileCount);
+				this.uploads[uploadItem.ID] = uploadItem;
+				//this.uploads[1] = 3;
+				
+				if(this.fileCount == 1){
+					this.uploads[uploadItem.ID].start(this.file);
+					this.clickHandlers();
+				}
+				
+				//alert(this.uploads);
 			},
 			
-			renderForm: function() {
-				
-				var name= "string";
-				
-				$('#upload').append('<form id="uploadForm-'+name+'" enctype="multipart/form-data" method="post" name="fileinfo" >'+
-						  '<label>File to upload:</label><input id="inputFile" type="file" name="file[]" required /></form>' +
-						  '<div id="output"></div><input id="clickMe" type="button" value="Upload File" onclick="sendForm();" />'+
-						  '<span id="stopButton">Stop</span><span id="addUpload" style="color:red">Add upload form</span>');
-				
-			},
+
 			
 			clickHandlers: function(){
 				
@@ -187,17 +262,77 @@ var UploadController = function() {
 			stop: function (){
 				this.uploading = false;
 				alert("stop button pressed");
+			},
+			
+			onclick : function(){
+				
+			},
+			
+			complete: function(fileItem) {
+				
+				delete this.uploads[fileItem.ID];
+				//initiate next item in queue
+				
+				if(this.uploads[this.fileCount+1] !== null && this.uploads[this.fileCount+1] !== undefined){
+					this.uploads[this.fileCount+1].start();
+				}
+				alert(fileItem.ID + " file has completed");
+				
+				//fileItem.priority;
 			}
+			
 	};
 
-function sendForm(state){
-		var fileInput =  document.getElementById("inputFile");
+function sendForm(evt) {
+	var form = $(this).parent();
+	
+	var fileInput = form.find('input[name="file"]')[0];
+	
+	var uploadID = form.find('input[name="uploadID"]').val();
+	
+	console.log(uploadID);
+	return;
+	
+		var currID = $("#uploadButton").siblings().attr('rel:fileInput');
+		alert(currID);
+	//	var fileInput =  document.getElementById("inputFile-" + currID);
 		var files = fileInput.files;
 		if (files.length === 0){
 			console.log('No input file found');
 			return;
 		}
-		file = files[0];
-		var upload = new UploadController;
+		var file = files[0];
 		upload.queue(file);
 }
+
+
+$().ready(function() {
+	window.upload = new UploadController();
+	
+	var uploadItems = [];
+	window.uploadID = 1;
+	
+	/*
+	$('form.uploadForm').each(function() {
+		
+		var item = new UploadItem({
+			form: $(this),
+			delegate: window.upload,
+			id: uploadID
+		});
+		uploadID++;
+		
+		uploadItems.push(item);
+	});
+	*/
+	
+	for (var i=0; i< 3; i++) {
+		var item = new UploadItem({
+			delegate: window.upload,
+			id: uploadID
+		});
+		uploadID++;
+		
+		uploadItems.push(item);
+	}
+});
